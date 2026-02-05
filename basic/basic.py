0293ea31ce0749212122eb15d747019253968b76
김건우 <kantenna11@gmail.com>
2026-02-05 18:07:53 +0900

    파이썬 타입

---
+++ basic/basic.py
@@ -0,0 +1,16 @@
+# 주석
+print("파이썬 프로그래밍")
+
+str1 = "Life blooms like a flower"
+str1 = "Life blooms like a flower"
+str1 = """Life blooms like a flower"""
+str1 = """Life blooms like a flower"""
+
+print(str1)
+
+
+str1 = """Life blooms 
+like a flower"""
+
+print(str1)
+
